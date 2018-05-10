852d65658f217c5c0e8191d835fb571db426d6dc
Junrill Galvez <junrill@nzfsg.co.nz>
2018-05-10 18:16:02 +0800

    fix AddTodo component

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,52 +1,41 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-export default class AddTodo extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() => {
-      this.forceUpdate();
-    });
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const { store } = this.context;
-    const state = store.getState();
-    return (
-      <div className="input-group">
-        <input
-          ref={node => {
-            this.input = node;
+const AddTodo = (props, { store }) => {
+  let input;
+  let todoxIndex = 0;
+  return (
+    <div className="input-group">
+      <input
+        ref={node => {
+          input = node;
+        }}
+        type="text"
+        className="form-control"
+        placeholder="Write Something..."
+      />
+      <div className="input-group-append">
+        <button
+          className="btn btn-info"
+          type="button"
+          onClick={() => {
+            store.dispatch({
+              type: "ADD_TODO",
+              id: todoxIndex++,
+              text: input.value
+            });
+            input.value = "";
           }}
-          type="text"
-          className="form-control"
-          placeholder="Write Something..."
-        />
-        <div className="input-group-append">
-          <button
-            className="btn btn-info"
-            type="button"
-            onClick={() => {
-              store.dispatch({
-                type: "ADD_TODO",
-                id: state.todos.length,
-                text: this.input.value
-              });
-              this.input.value = "";
-            }}
-          >
-            ADD
-          </button>
-        </div>
+        >
+          ADD
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 AddTodo.contextTypes = {
   store: PropTypes.object
 };
+
+export default AddTodo;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./components/App";
-import Provider from "./components/Provider";
 import todoApp from "./reducers/todoApp";
 
 render(
