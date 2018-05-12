551e2a65ca1e5e8294a31364818ad24c8c4b03f9
Junrill Galvez <junrill@nzfsg.co.nz>
2018-05-12 08:14:03 +0800

    static image

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import AddTodo from "./AddTodo";
 import TodoList from "./TodoList";
 import FilterLinkList from "./FilterLinkList";
-import logoURL from "../assets/logo.png";
 
 export default () => {
   return (
     <div className="container text-center">
-      <img src={logoURL} className="img-fluid my-5" />
+      <img
+        src="https://uploads.codesandbox.io/uploads/user/7c44c586-da4b-4bf1-8f95-3485f6673811/3REH-logo.png"
+        className="img-fluid my-5"
+      />
       <AddTodo />
       <FilterLinkList />
       <TodoList />
